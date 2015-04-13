3420732b371425c1e029f9d1a529319cb53797a7
Marian Triebe <m.triebe@live.de>
2015-04-13 20:33:49 +0200

    Refactored main.java files

--- VerkehrschaosTruckCompany/src/verkehrschaosTruckCompany/companyStarter.java
+++ VerkehrschaosTruckCompany/src/verkehrschaosTruckCompany/companyStarter.java
@@ -1,4 +1,4 @@
-package verkehrschaosTruck;
+package verkehrschaosTruckCompany;
 
 import java.util.Properties;
 
@@ -15,42 +15,46 @@ import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
 import org.omg.PortableServer.POAPackage.ServantNotActive;
 import org.omg.PortableServer.POAPackage.WrongPolicy;
 
-import verkehrschaos.Truck;
-import verkehrschaos.TruckHelper;
+import verkehrschaos.TruckCompany;
+import verkehrschaos.TruckCompanyHelper;
 
-public class main {
+public class companyStarter {
 	public static void main(String[] args) {
 		Properties props = new Properties();
 		props.put("org.omg.CORBA.ORBInitialPort", "20000");
 		props.put("org.omg.CORBA.ORBInitialHost", "localhost");
 		// Get input for company name and comapany location
-		String truck_name    = "";
-		String company_name  = "";
+		String company_name = "";
+		String company_pos  = "";
 		for (int i = 0; i < args.length; ++i) {
 			if (args[i].contains("--name=")) {
 				String[] splitted = args[i].split("=");
-				truck_name = splitted.length == 2 ? splitted[1] : "";
+				company_name = splitted.length == 2 ? splitted[1] : "";
 			}
-			if (args[i].contains("--company=")) {
+			if (args[i].contains("--location=")) {
 				String[] splitted = args[i].split("=");
 				company_name = splitted.length == 2 ? splitted[1] : "";
 			}
 			if (args[i].contains("--help")) {
-				System.out.println("Usage: java -cp . verkehrschaosTruck [OPTIONS...]");
+				System.out.println("Usage: java -cp . verkehrschaosTruckCompany [OPTIONS...]");
 				System.out.println("Arguments:");
-				System.out.println("--name=arg      Set the truck name");
-				System.out.println("--company=arg   Set the truck company");
+				System.out.println("--name=arg      Set the company name");
+				System.out.println("--location=arg  Set the location of the company");
+				System.out.println("                Valid locations: nord, ost, sued, west");
 				System.out.println("--help          Print this help message");
+				System.out.println("ORB Arguments are passed to CORBA Framework");
 				System.exit(-1);
 			}
 		}
 		// Check input
-		if (truck_name.isEmpty()) {
-			System.out.println("No truck name given! Set truck name with `--name=...`");
+		if (company_name.isEmpty()) {
+			System.out.println("No company name given! Set copmany name with `--name=...`");
 			System.exit(-2);
 		}
-		if (company_name.isEmpty()) {
-			System.out.println("No company name given! Set company name with `--company=...");
+		if (company_pos != "nord" || company_pos != "sued" || company_pos != "west"
+			|| company_pos != "ost") {
+			System.out.println("No valid location specified! Set location with `--location=...`");
+			System.out.println("Valid locations are: nord, sued, west, ost");
 			System.exit(-3);
 		}
 		// Init ORB
@@ -60,38 +64,30 @@ public class main {
 			final POA rootPoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
 			rootPoa.the_POAManager().activate();
 			// Init our object
-			final TruckImpl truck = new TruckImpl(truck_name);
+			final TruckCompanyImpl company = new TruckCompanyImpl(company_name);
 			// Get reference to our servant
-			org.omg.CORBA.Object ref = rootPoa.servant_to_reference(truck);
-		    final Truck href = TruckHelper.narrow(ref);
-		    truck.setTruck(href);
+			org.omg.CORBA.Object ref = rootPoa.servant_to_reference(company);
+		    final TruckCompany href = TruckCompanyHelper.narrow(ref);
+		    company.setObj(href);
 		    // Get name service 
 		    final NamingContextExt nc = NamingContextExtHelper.narrow(
 		    		orb.resolve_initial_references("NameService"));
 		    // bind our object ref to a name
-		    final NameComponent path[] = nc.to_name(truck_name);
+		    final NameComponent path[] = nc.to_name(company_name);
 		    nc.rebind(path, href);
 		    // Register for location and start our duty...
 		    // This call is blocking until program has to exit
-		    truck.run(nc, company_name);
+		    company.run(nc, company_pos);
 		    // unbind object from name service
 		    nc.unbind(path);
-		    // SHutdown ORB
-		    orb.shutdown(true);
-		} catch (InvalidName e) {
+		} catch (InvalidName | AdapterInactive | ServantNotActive |
+				WrongPolicy | NotFound | CannotProceed |
+				org.omg.CosNaming.NamingContextPackage.InvalidName e) {
+			System.err.println(e.getMessage());
 			e.printStackTrace();
-		} catch (AdapterInactive e) {
-			e.printStackTrace();
-		} catch (ServantNotActive e) {
-			e.printStackTrace();
-		} catch (WrongPolicy e) {
-			e.printStackTrace();
-		} catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
-			e.printStackTrace();
-		} catch (NotFound e) {
-			e.printStackTrace();
-		} catch (CannotProceed e) {
-			e.printStackTrace();
+		} finally {
+		    // Shutdown ORB
+			orb.shutdown(true);
 		}
 	}
 }
