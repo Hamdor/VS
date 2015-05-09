7adb33d403d48aaaf7882550096c1f7f17142bb8
Marian Triebe <m.triebe@live.de>
2015-05-09 18:28:13 +0200

    Add some logic to `coordinatorImpl`

--- A2/projects/coordinator/src/coordinator/coordinatorImpl.java
+++ A2/projects/coordinator/src/coordinator/coordinatorImpl.java
@@ -1,11 +1,35 @@
 package coordinator;
 
+import java.util.ArrayList;
+
 public class coordinatorImpl extends CoordinatorPOA {
+	
+	private String m_name;
+	private ArrayList<String> m_workers;
+	private ArrayList<String> m_starters;
+	
+	public coordinatorImpl(final String name) {
+		m_name     = name;
+		m_workers  = new ArrayList<String>();
+		m_starters = new ArrayList<String>();
+	}
 
 	@Override
 	public void register(String whom) {
-		// TODO Auto-generated method stub
-		
+		synchronized(m_workers) {
+			if (!m_workers.contains(whom)) {
+				m_workers.add(whom);
+			}
+		}
+	}
+	
+	@Override
+	public void register_starter(String whom) {
+		synchronized(m_starters) {
+			if (!m_starters.contains(whom)) {
+				m_starters.add(whom);
+			}
+		}
 	}
 
 	@Override
@@ -16,15 +40,15 @@ public class coordinatorImpl extends CoordinatorPOA {
 
 	@Override
 	public String[] getStarter() {
-		// TODO Auto-generated method stub
-		return null;
+		synchronized(m_workers) {
+		    return m_workers.toArray(new String[m_workers.size()]);
+		}
 	}
 
 	@Override
 	public void calculate(String monitor, int ggTLower, int ggTUpper,
 			int delayLower, int delayUpper, int period, int expectedggT) {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -32,5 +56,4 @@ public class coordinatorImpl extends CoordinatorPOA {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
