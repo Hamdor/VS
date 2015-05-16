b9e8f18974a836a17cc723296f459ef06c10b97a
Marian Triebe <m.triebe@live.de>
2015-05-16 13:23:18 +0200

    Remove tabs

--- A2/projects/client/src/main_starter/main_starter.java
+++ A2/projects/client/src/main_starter/main_starter.java
@@ -2,7 +2,7 @@ package main_starter;
 
 public class main_starter {
 
-	public static void main(String[] args) {
-		
-	}
+  public static void main(String[] args) {
+
+  }
 }
--- A2/projects/worker/src/worker/workerImpl.java
+++ A2/projects/worker/src/worker/workerImpl.java
@@ -3,53 +3,53 @@ package worker;
 import java.util.concurrent.SynchronousQueue;
 
 public class workerImpl extends WorkerPOA {
-	private String m_name;
-	private Thread m_thread;
-	private SynchronousQueue<Job> m_jobs;
-	private boolean run = true;
-	
-	public workerImpl(final String name) {
-		m_name = name;
-		m_jobs = new SynchronousQueue<Job>();
-		m_thread = new Thread(new Runnable() {
-	        @Override
-	        public void run() {
-	        	while(run) {
-	        		try {
-						Job current_job = m_jobs.take();
-						// TODO
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-	        	}
-	        }
-	    });
-		m_thread.start();
-	}
-	
-	@Override
-	public void init(String left, String right, int value, int delay,
-			String monitor) {
-		// TODO Auto-generated method stub
-		
-	}
+  private String m_name;
+  private Thread m_thread;
+  private SynchronousQueue<Job> m_jobs;
+  private boolean run = true;
 
-	@Override
-	public void shareResult(String sender, int value) {
-		// TODO Auto-generated method stub
-		
-	}
+  public workerImpl(final String name) {
+    m_name = name;
+    m_jobs = new SynchronousQueue<Job>();
+    m_thread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        while (run) {
+          try {
+            Job current_job = m_jobs.take();
+            // TODO
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    });
+    m_thread.start();
+  }
 
-	@Override
-	public void kill() {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public void init(String left, String right, int value, int delay,
+      String monitor) {
+    // TODO Auto-generated method stub
 
-	@Override
-	public void snapshot(String sender) {
-		// TODO Auto-generated method stub
-		
-	}
+  }
+
+  @Override
+  public void shareResult(String sender, int value) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void kill() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void snapshot(String sender) {
+    // TODO Auto-generated method stub
+
+  }
 
 }
