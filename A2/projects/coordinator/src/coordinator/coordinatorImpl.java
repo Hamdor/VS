a1b9c558b08b8dc4a54c1280bb6d0fee7b24529f
Marian Triebe <m.triebe@live.de>
2015-05-17 00:02:14 +0200

    Add starter to coordinator

--- A2/projects/coordinator/src/coordinator/coordinatorImpl.java
+++ A2/projects/coordinator/src/coordinator/coordinatorImpl.java
@@ -3,76 +3,88 @@ package coordinator;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
+import java.util.concurrent.Semaphore;
 
 public class coordinatorImpl extends CoordinatorPOA {
-	
-	private String m_name;
-	private HashMap<String, ArrayList<String>> m_registry;
-	
-	public coordinatorImpl(final String name) {
-		m_name    = name;
-		m_registry = new HashMap<String, ArrayList<String>>();
-	}
-	
-	@Override
-	public void register(String whom, String owner) {
-		synchronized(m_registry) {
-			ArrayList<String> workerList = m_registry.get(owner);
-			if (workerList == null) {
-				// if this happens the starter didn't register itself to
-				// the coordinator. This should never happen...
-				System.out.println("[WARNING]: Register Worker, but Starter was not known before...");
-				workerList = new ArrayList<String>();
-				m_registry.put(owner, workerList);
-			}
-			workerList.add(whom);
-		}
-	}
-	
-	@Override
-	public void register_starter(String whom) {
-		synchronized(m_registry) {
-			if (m_registry.get(whom) == null) {
-				m_registry.put(whom, new ArrayList<String>());
-			}
-		}
-	}
 
-	@Override
-	public void inform(String whom, int seqNr, boolean finished, int current) {
-		// TODO Auto-generated method stub
-		
-	}
+  private String m_name;
+  private HashMap<String, ArrayList<String>> m_registry;
+  private Semaphore m_sema;
 
-	@Override
-	public String[] getStarter() {
-		synchronized(m_registry) {
-		    //return m_registry.toArray(new String[m_registry.size()]);
-			Set<String> keys = m_registry.keySet();
-			String[] result = new String[keys.size()];
-			int idx = 0;
-			for (String s : keys) {
-				result[idx++] = s;
-			}
-			return result;
-		}
-	}
+  public coordinatorImpl(final String name) {
+    m_name = name;
+    m_registry = new HashMap<String, ArrayList<String>>();
+    m_sema = new Semaphore(0);
+  }
 
-	@Override
-	public void calculate(String monitor, int ggTLower, int ggTUpper,
-			int delayLower, int delayUpper, int period, int expectedggT) {
-		// TODO Auto-generated method stub
-	}
+  public void run() {
+    try {
+      m_sema.acquire();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 
-	@Override
-	public void kill(String whom) {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public void register(String whom, String owner) {
+    synchronized (m_registry) {
+      ArrayList<String> workerList = m_registry.get(owner);
+      if (workerList == null) {
+        // if this happens the starter didn't register itself to
+        // the coordinator. This should never happen...
+        System.out
+            .println("[WARNING]: Register Worker, but Starter was not known before...");
+        workerList = new ArrayList<String>();
+        m_registry.put(owner, workerList);
+      }
+      workerList.add(whom);
+    }
+  }
 
-	@Override
-	public void terminate() {
-		// TODO Auto-generated method stub
-		
-	}
+  @Override
+  public void register_starter(String whom) {
+    synchronized (m_registry) {
+      if (m_registry.get(whom) == null) {
+        m_registry.put(whom, new ArrayList<String>());
+      }
+    }
+  }
+
+  @Override
+  public void inform(String whom, int seqNr, boolean finished, int current) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public String[] getStarter() {
+    synchronized (m_registry) {
+      // return m_registry.toArray(new String[m_registry.size()]);
+      Set<String> keys = m_registry.keySet();
+      String[] result = new String[keys.size()];
+      int idx = 0;
+      for (String s : keys) {
+        result[idx++] = s;
+      }
+      return result;
+    }
+  }
+
+  @Override
+  public void calculate(String monitor, int ggTLower, int ggTUpper,
+      int delayLower, int delayUpper, int period, int expectedggT) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void kill(String whom) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void terminate() {
+    // TODO: Kill all starters
+    m_sema.release();
+  }
 }
--- A2/projects/coordinator/src/main_starter/main_starter.java
+++ A2/projects/coordinator/src/main_starter/main_starter.java
@@ -1,8 +1,99 @@
 package main_starter;
 
+import java.util.Properties;
+
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+
+import coordinator.coordinatorImpl;
+
 public class main_starter {
+  static final long sleep_time = 500; // Sleep for 500ms before ORB.shutdown()
+
+  ORB m_orb = null;
+  POA m_rootpoa = null;
+  NamingContextExt m_nameingcontext = null;
+
+  private static void print_help_message() {
+    StringBuilder str = new StringBuilder();
+    str.append("Usage: java -cp . client [Options...]\n");
+    str.append("Arguments:\n");
+    str.append("--name=arg          Set the coordinator name\n");
+    str.append("--build-in-props    Use the build in ORB arguments\n");
+    str.append("--help              Print this help message\n");
+    str.append("ORB Arguments are passed to CORBA Framework");
+    System.out.println(str);
+  }
+
+  private static String read_argument(final String line) {
+    String[] splitted = line.split("=");
+    return splitted.length == 2 ? splitted[1] : "";
+  }
+
+  private boolean initCorba(final Properties props, final String[] args,
+      final String coordinator) {
+    boolean init = true;
+    m_orb = ORB.init(args, props);
+    try {
+      m_rootpoa = POAHelper.narrow(m_orb.resolve_initial_references("RootPOA"));
+      m_rootpoa.the_POAManager().activate();
+      m_nameingcontext = NamingContextExtHelper.narrow(m_orb
+          .resolve_initial_references("NameService"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      init = false;
+    } finally {
+      if (init) {
+        // Add shutdown hook if CORBA was initialized successfully
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+          public void run() {
+            shutdown();
+          }
+        });
+      }
+    }
+    return init;
+  }
 
   public static void main(String[] args) {
+    Properties props = null;
+    String coordinator_name = "";
+    for (int i = 0; i < args.length; ++i) {
+      if (args[i].contains("--name=")) {
+        coordinator_name = read_argument(args[i]);
+      }
+      if (args[i].contains("--build-in-props")) {
+        props = new Properties();
+        props.put("org.omg.CORBA.ORBInitialPort", "20000");
+        props.put("org.omg.CORBA.ORBInitialHost", "localhost");
+      }
+      if (args[i].contains("--help")) {
+        print_help_message();
+        System.exit(0);
+      }
+    }
+    // Check input
+    if (coordinator_name.isEmpty()) {
+      print_help_message();
+      System.exit(-1);
+    }
+    final main_starter instance = new main_starter();
+    if (!instance.initCorba(props, args, coordinator_name)) {
+      System.out.println("Error initializing CORBA...");
+    }
+    coordinatorImpl coord = new coordinatorImpl(coordinator_name);
+    coord.run();
+  }
 
+  private void shutdown() {
+    try {
+      Thread.sleep(sleep_time);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    m_orb.shutdown(true);
   }
 }
