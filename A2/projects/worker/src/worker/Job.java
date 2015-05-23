2e1d88764a843da7ec6bf957c2d81f8e0d124357
Marian Triebe <m.triebe@live.de>
2015-05-23 16:10:39 +0200

    Remove tabs

--- A2/projects/worker/src/worker/Job.java
+++ A2/projects/worker/src/worker/Job.java
@@ -5,8 +5,15 @@ public class Job {
     m_value = value;
     m_marker = marker;
   }
-	private int m_value = 0;
-	private boolean m_marker = false;
-	public int value() { return m_value; }
-	public boolean marker() { return m_marker; }
+
+  private int m_value = 0;
+  private boolean m_marker = false;
+
+  public int value() {
+    return m_value;
+  }
+
+  public boolean marker() {
+    return m_marker;
+  }
 }
