820e93f037b60a3e9c4b4bcfa3516ddbddde2767
Marian Triebe <m.triebe@live.de>
2015-06-13 17:02:24 +0200

    Add missing header guard to generic_guard.hpp
    Also added small debug output

--- A4/include/generic_guard.hpp
+++ A4/include/generic_guard.hpp
@@ -15,15 +15,29 @@
  * File: generic_guard.hpp                                                    *
  ******************************************************************************/
 
+#ifndef GENERIC_GUARD_HPP
+#define GENERIC_GUARD_HPP
+
+#define DEBUG
+
+#ifdef DEBUG
+#include <iostream>
+#endif
+
 template<typename F, typename X>
 struct generic_guard {
   generic_guard(F fun, X a) : m_arg(a), m_fun(fun) {
-    // nop
+#ifdef DEBUG
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+#endif
   }
   generic_guard(const generic_guard&) = delete;
   generic_guard(generic_guard&&) = default;
   ~generic_guard() {
     m_fun(m_arg);
+#ifdef DEBUG
+    std::cout << __PRETTY_FUNCTION__ << std::endl; 
+#endif
   }
 private:
   X m_arg;
@@ -34,4 +48,6 @@ template<typename F, typename X>
 auto make_guard(F f, X x) {
   return generic_guard<F, X>(f,x);
 }
+
+#endif // GENERIC_GUARD_HPP
 
